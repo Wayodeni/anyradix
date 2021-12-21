5ad9c2f021e15287bcce632f4c44f7368666082c
Wayodeni <levgeneral@yandex.ru>
2021-12-21 21:38:43 +0300

    Add proper comments Shortened lines with 79 symbols per line

--- any_radix_backend.py
+++ any_radix_backend.py
@@ -2,47 +2,71 @@
 В этом файле содержится класс переводчика из одной СС в другую.
 Чтобы не засорять фронтенд логикой программы, ее класс вынесен в этот файл.
 
-Конструктор класса принимает на вход число, основание его СС, и СС результирующего числа.
-Для расчета результата вызывается метод translate(), который возвращает переменную типа string.
+Конструктор класса принимает на вход число, основание его СС, и СС результирую-
+щего числа. Для расчета результата вызывается метод translate(), который возвр-
+ащает переменную типа string.
 '''
 # TODO: Зарефакторить "исключения". Вынести их в отдельный строковый метод.
 import string
 
 
 class Translator():
-    def __init__(self, input_number = '0', input_number_radix = 10, result_radix = 10):
+    def __init__(self, input_number = '0', input_number_radix = 10,
+     result_radix = 10):
+        '''
+        Конструктор класса Translator.
+        Задает необходимые атрибуты, которые позже будут обрабатываться внутри 
+        метода translate.
+        '''
         self.input_number = str(input_number).upper()
         self.input_number_radix = input_number_radix
         self.result_radix = result_radix
 
 
     def translate(self) -> str:
-        # Если пустой ввод, то кидаем NoInputException
-        if self.input_number == '' or self.input_number_radix == '' or self.result_radix == '':
-            return 'NoInputException'
+        '''
+        Метод отвечает за валидацию входных данных и перевод числа.
+        Возвращает либо строку с описанием ошибки, либо строку содержащую резу-
+        льтат.
+        '''
 
-        # Если в унарной сс есть лишние символы или в других сс есть лишние символы,
-        # а также если есть лишние символы в полях оснований сс, то кидаем RedundantSymbolsException.
-        if (self.input_number.isalnum() == False and set(self.input_number) != {'|'})\
+        # Если пустой ввод хотя бы в одном из полей, то в поле результата выво-
+        # дим 'Пустое поле ввода'.
+        if self.input_number == '' or self.input_number_radix == '' or \
+            self.result_radix == '':
+            return 'Пустое поле ввода'
+
+        # Если в унарной СС есть лишние символы или в других СС есть лишние 
+        # символы, а также если есть лишние символы в полях оснований СС, то в
+        # поле результата выводим 'Удалите лишние символы'.
+        if (self.input_number.isalnum() == False\
+            and set(self.input_number) != {'|'})\
             or str(self.input_number_radix).isdigit() == False\
             or str(self.result_radix).isdigit() == False:
-            return 'RedundantSymbolsException'
+            return 'Удалите лишние символы'
 
-        # После проверки оснований сс на то, что они состоят только из чисел, можем преобразовать их в int.
+        # После проверки оснований СС на то, что они состоят только из чисел, 
+        # можем преобразовать их в int.
         self.input_number_radix = int(self.input_number_radix)
         self.result_radix = int(self.result_radix)
 
-        # Если любое из оснований сс превышает диапазон 1-37, то кидаем TooBigRadixException
-        if self.input_number_radix not in range(1, 37) or self.result_radix not in range(1, 37):
-            return 'WrongRadixException'
+        # Если любое из оснований сс превышает диапазон 1-37, то в поле 
+        # результата выводим 'Неверное основание системы счисления'
+        if self.input_number_radix not in range(1, 37) or self.result_radix\
+            not in range(1, 37):
+            return 'Неверное основание системы счисления'
 
-        # Если на вход было подано число в унарной сс, но указана другая сс, то кидаем UnarySystemRadixException.
+        # Если на вход было подано число в унарной сс, но указана другая сс, 
+        # то в поле результата выводим 'Неверная система счисления для унарного
+        # числа'
         if set(self.input_number) == {'|'} and self.input_number_radix != 1:
-            return 'UnarySystemRadixException'
+            return 'Неверная система счисления для унарного числа'
 
-        # Если число превышает основание своей сс, кидаем RadixExceedationException.
-        if Translator.is_exceeds_radix(self.input_number, self.input_number_radix) == True:
-            return 'RadixExceedationException'
+        # Если число превышает основание своей сс, то в поле результата выводим
+        # 'Введенное число превышает свою систему счисления'
+        if Translator.is_exceeds_radix(self.input_number, 
+        self.input_number_radix) == True:
+            return 'Введенное число превышает свою систему счисления'
 
         # Нуль во всех сс есть нуль.
         if self.input_number == '0':
@@ -54,7 +78,9 @@ class Translator():
         elif self.input_number_radix == 10:
             input_number_in_10th_radix = int(self.input_number)
         else:
-            input_number_in_10th_radix = Translator.translate_to_10th_radix(self.input_number, self.input_number_radix)
+            input_number_in_10th_radix =\
+            Translator.translate_to_10th_radix(self.input_number, 
+            self.input_number_radix)
         
         # Формирование результата
         n = input_number_in_10th_radix
@@ -68,13 +94,22 @@ class Translator():
             return result
         else:
             while n:
-                result = Translator.number_to_literal(n % self.result_radix) + result
+                result = Translator.number_to_literal(n % self.result_radix)\
+                + result
                 n //= self.result_radix
             return result
 
 
     @staticmethod
     def translate_to_10th_radix(input_number: str, input_number_radix: int) -> int:
+        '''
+        Создаем статический метод т.к. его можно использовать позже в других 
+        модулях для абсолютно любых переводов. После всех проведенных проверок,
+        совершаемых на этапе вызова метода translate(), данный метод принимает
+        число в строковом виде из поля ввода, его систему счисления и переводит
+        его в 10 СС (приведение к стандартному виду).
+        '''
+
         # Для унарной СС
         if input_number_radix == 1:
             number_in_10th_radix = input_number.count('|')
@@ -88,18 +123,24 @@ class Translator():
                 power_of_radix -= 1 
         elif input_number_radix in range(11, 37):
             for literal in input_number:
-                number_in_10th_radix += int(Translator.literal_to_number(literal)) * input_number_radix**power_of_radix
+                number_in_10th_radix += int(Translator.literal_to_number(literal))\
+                    * input_number_radix**power_of_radix
                 power_of_radix -= 1 
         return number_in_10th_radix
 
 
     @staticmethod
     def literal_to_number(literal: str) -> int:
+        '''
+        Данный метод вызывается при поразрядном парсинге числа и переводит
+        буквы в числах (СС >= 11) в цифры в 10 СС.
+        '''
         # Если на вход пришла цифра не в буквенном виде, то возвращаем ее. 
         if literal.isdigit():
             return int(literal)
 
-        # Генерация словаря, в котором содержится соответствие букв английского алфавита цифрам в 10 СС
+        # Генерация словаря, в котором содержится соответствие букв английского
+        # алфавита цифрам в 10 СС
         alphabet = {}
         number = 10
         for letter in string.ascii_uppercase:
@@ -111,11 +152,19 @@ class Translator():
 
     @staticmethod
     def number_to_literal(numeral: int) -> str:
-        # Если на вход пришла цифра меньше 10, то для нее не будет буквы в СС > 10. Преобразовываем в str и возвращаем.
+        '''
+        Данный метод вызывается при поразрядном парсинге числа и 
+        преобразовывает числа от 10 и далее в буквенные разряды для СС
+        больше 10.
+        '''
+
+        # Если на вход пришла цифра меньше 10, то для нее не будет буквы в 
+        # СС > 10. Преобразовываем в str и возвращаем.
         if numeral < 10:
             return str(numeral)
 
-        # Генерация словаря, в котором содержится соответствие цифр буквам английского алфавита
+        # Генерация словаря, в котором содержится соответствие цифр буквам 
+        # английского алфавита
         alphabet = {}
         number = 10
         for letter in string.ascii_uppercase:
@@ -127,6 +176,12 @@ class Translator():
 
     @staticmethod
     def is_exceeds_radix(input_number: str, input_number_radix: int) -> bool:
+        '''
+        Данный метод проверяет число на соответствие указанной для него системе
+        счисления. Если число содержит цифру, превышающую основание системы 
+        счисления, то возвращается True. В противном случае возвращается False.
+        '''
+
         # Для унарной СС
         if set(input_number) == {'|'}:
             return False
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from PyQt5 import QtWidgets
-import any_radix_frontend
-from any_radix_backend import Translator
-import sys
+from PyQt5 import QtWidgets  # Импортируем набор виджетов (элементы интерфейса) Qt5
+import any_radix_frontend  # Импортируем файл с "версткой" GUI. (Переконвертированный .ui файл)
+from any_radix_backend import Translator  # Импортируем класс с логикой программы
+import sys  # Импортируем модуль sys для вызова функции выхода
 
-
+# Наследуем наш класс AnyRadix от классов QtWidgets.QMainWindow, any_radix_frontend.Ui_MainWindow
+# Получаем доступ к методам и атрибутам родительского класса
 class AnyRadix(QtWidgets.QMainWindow, any_radix_frontend.Ui_MainWindow):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+        super().__init__()  # Вызов конструктора родительского класса
+        self.setupUi(self)  # Настройка виджета (В данном случае нашего окна)
 
+        # Привязка методов к кнопкам
         self.translateButton.clicked.connect(self.translate)
         self.clearButton.clicked.connect(self.clear_fields)
         self.fileExitAction.triggered.connect(sys.exit)
         self.aboutAction.triggered.connect(self.info)
 
+    # Вызов метода перевода из класса Translate. Заполнение поля результата
     def translate(self):
         input_num = self.inputNumField.text()
         input_num_radix = self.inputNumRadixField.text()
@@ -21,16 +24,18 @@ class AnyRadix(QtWidgets.QMainWindow, any_radix_frontend.Ui_MainWindow):
         calc = Translator(input_num, input_num_radix, result_radix)
         self.resultField.setText(calc.translate())
 
+    # Вызов диалогового окна со справкой
     def info(self):
         QtWidgets.QMessageBox.about(self, 'Автор', 'Программа была написана студентом\nФИО')
 
+    # Метод, очищающий поля
     def clear_fields(self):
         self.inputNumField.setText('')
         self.inputNumRadixField.setText('')
         self.resultRadixField.setText('')
         self.resultField.setText('')
 
-
+# Создание и показ окна
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = AnyRadix()
